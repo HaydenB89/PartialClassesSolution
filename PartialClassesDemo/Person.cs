64b6645ca220cb848427fc7423cc677392592e68
hayden brown <haydenbrowncinci@gmail.com>
2022-02-09 09:38:28 -0500

    Add project files.

---
+++ PartialClassesDemo/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassesDemo {
+    public class Person {
+        public string Name { get; set; }
+
+    }
+
+    public partial class Person {
+        public string GetName() {
+            return Name;
+        }
+    }
+}
---
+++ PartialClassesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PartialClassesDemo {
+    class Program {
+        static void Main(string[] args) {
+            var person = new Person();
+            var name = person.GetName();
+        }
+    }
+}
